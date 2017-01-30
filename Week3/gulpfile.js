c6204e9066ab927516b4be27b1cca2f1517fd8fa
vaibhav <weatherapprobux@gmail.com>
2017-01-31 01:21:02 +0530

    Add Week3 Excercise 2: Angular templates
    The ng-include won't show anything unless you load it using a local server.
    If you can't see the included content in the index.html page, that is because you are facing an XMLHttpRequest problem, your browser cannot load cross-origin requests.
    
    The answer to this problem:
    
    1) You can launch Chrome with --allow-file-access-from-files parameter to disable checks for file://.
    
    2) Change your gulpfile.js:
    
    2.1) In the usemin task change to gulp.src('app/*.html') ...
    
    2.2) Change browserSync.init to:
    	browserSync.init(files, {
      server: {
        baseDir: 'dist',
        index: 'index.html'
      },
      reloadDelay: 1000
    });
    
    Yoy can find this solution in Week3 forum.

--- Week3/gulpfile.js
+++ Week3/gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/index.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [uglify(),rev()]
@@ -74,18 +74,18 @@ gulp.task('browser-sync', ['default'], function () {
       'dist/**/*'
    ];
 
-   browserSync.init(files, {
-      server: {
-         baseDir: "dist",
-         index: "menu.html"
-      }
-   });
-        // Watch any files in dist/, reload on change
+browserSync.init(files, {
+  server: {
+    baseDir: 'dist',
+    index: 'index.html'
+  },
+  reloadDelay: 1000
+});        // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', browserSync.reload);
     });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/*.html')
     .pipe(usemin({
       css:[minifycss(),rev()],
       js: [ngannotate(),uglify(),rev()]
